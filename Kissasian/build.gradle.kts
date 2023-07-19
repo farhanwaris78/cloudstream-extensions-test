ea280be1baba207f8aa0db12c5566dace4e95df3
farhanwaris78 <55206873+farhanwaris78@users.noreply.github.com>
2023-07-19 19:14:13 +0500

    Update build.gradle.kts

--- Kissasian/build.gradle.kts
+++ Kissasian/build.gradle.kts
@@ -1,5 +1,5 @@
 // use an integer for version numbers
-version = 2
+version = 1
 
 
 cloudstream {
@@ -7,7 +7,7 @@ cloudstream {
     // All of these properties are optional, you can safely remove them
 
     // description = "Lorem Ipsum"
-     authors = listOf("Hexated")
+     authors = listOf("FarhanWaris78")
 
     /**
      * Status int as the following:
